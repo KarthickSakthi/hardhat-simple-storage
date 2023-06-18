da1b30e348a1e0a871c0188fcb9725b8a00f24c8
Karthick S <karthicksakthi124@gmail.com>
2023-06-18 11:00:40 +0000

    config changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  defaultNetwork: "hardhat",
+  networks:{
+    mumbai: {
+      url: "https://rpc-mumbai.matic.today", // Mumbai RPC endpoint
+      chainId: 80001, // Mumbai chain ID
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  }
 };
