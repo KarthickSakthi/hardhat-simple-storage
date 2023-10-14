faf8a685a6a31bee7fa7e2648a22004ce8579414
Karthick S <karthicksakthi124@gmail.com>
2023-10-14 10:03:31 +0000

    unit test case bug fix

--- test/Lock.js
+++ test/Lock.js
@@ -2,9 +2,8 @@ const {expect} = require("chai");
 const {ethers} = require("hardhat");
 
 describe("This is testing scope of Simple Store", function(){
+  let simpleStorage, signer ;
   beforeEach( async function(){
-    let simpleStorage, signer ;
-
     let simpleStorageFactory = await ethers.deployContract("SimpleStorage");
     simpleStorage = await simpleStorageFactory.waitForDeployment(6);
     signer = ethers.getSigner();
@@ -12,7 +11,7 @@ describe("This is testing scope of Simple Store", function(){
 
   it("intial value should be 0", async function(){
     let expectedValue ="0";
-    let currentStoredValue = await simpleStorage.retrive();
+    let currentStoredValue = await simpleStorage.retrieve();
     expect(currentStoredValue.toString()).to.equal(expectedValue);
   })
 
@@ -20,7 +19,7 @@ describe("This is testing scope of Simple Store", function(){
     let expectedValue ="1";
     let storeValue = await simpleStorage.store(1);
     await storeValue.wait(6);
-    let currentStoredValue = await simpleStorage.retrive();
+    let currentStoredValue = await simpleStorage.retrieve();
     expect(currentStoredValue.toString()).to.equal(expectedValue);
   })
 })
