c187dadf0b0e8b1ad8aaf41d3e6453861e456a6a
Karthick S <karthicksakthi124@gmail.com>
2023-10-14 09:57:05 +0000

    test contract changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-verify")
 require("dotenv").config();
+require("./task/block-number");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -8,12 +9,12 @@ module.exports = {
   defaultNetwork: "hardhat",
   networks:{
     mumbai: {
-      url: "https://rpc-mumbai.maticvigil.com", // sepolia RPC endpoint
+      url: "https://greatest-still-emerald.matic-testnet.discover.quiknode.pro/4ae6c079b0b708107fad711417ab6b6b3c580fbe/", // sepolia RPC endpoint
       chainId: 80001, // sepolia chain ID
       accounts: [""],
     }
-  },
-  etherscan:{
-    apiKey: ""
   }
+  // etherscan:{
+  //   apiKey: ""
+  // }
 };
